5f64d9089db9efbdf489f4358d2b97f12fc9b3c1
lilith <connor@inet-sys.com>
2015-07-06 20:06:03 +1000

    Added simple hello world

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#include <GLUT/freeglut.h>
+#else
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+#endif
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+        printf("Hello world\n");
+        return 0;
+    }
---
+++ make
@@ -0,0 +1,16 @@
+#!/usr/bin/make -f
+CC = g++
+CFLAGS  = -Wall
+PROG = Blocker
+
+SRCS = main.cpp
+
+LIBS = -lglut -lGLU -lGL
+
+all: $(PROG)
+
+$(PROG):	$(SRCS)
+	$(CC) $(CFLAGS) -o $(PROG) $(SRCS) $(LIBS)
+
+clean:
+	rm -f $(PROG) 
